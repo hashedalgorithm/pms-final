495f40965ff1dc95bbd3f7359f4b0fc16b1d1236
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2025-02-09 19:13:46 +0100

    IATW updated my password generator

--- src/services/passwordMethods.py
+++ src/services/passwordMethods.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+import re
+import random
 import hashlib
 import time
 from random import randint, shuffle
@@ -6,6 +9,15 @@ from typing import Union
 import requests
 from fastapi import HTTPException
 from models.policyModel import PolicyModel
+
+
+allowedLowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz"
+allowedUpperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+allowedNumbers = "1234567890"
+allowedSpecialCharacters = "!@#$%^&*()_+-={}[];:<>,./?ß!"
+iterations = 1000
+keyLength = 64
+randomSaltLength = 20
 
 
 def check_leaks_via_HIBP(password: str) -> int:
@@ -56,67 +68,178 @@ def validate_password(password: str, policy: PolicyModel) -> bool:
     return True
 
 
-def generate_passwords(batch_size: int, policy: PolicyModel) -> Union[list[str], None]:
-    generators = [
-        __getUpperAlpha,
-        __getAlpha,
-        __getNumber,
-        __getSymbol,
-    ]
+def generatePassword(
+    length: int,
+    upperCaseCharacters: int,
+    numbers: int,
+    specialCharacters: int
+) -> str:
+    firstPasswordDraft = ""
+    for _ in range(length):
+        randomAllowedCharacterOrNumber = generateRandomAllowedCharacterOrNumber()
+        firstPasswordDraft += randomAllowedCharacterOrNumber
 
-    passwords: list[str] = []
+    patchedPassword = firstPasswordDraft
+    while True:
+        isLowerCaseCharactersValid, isNumbersValid, isSpecialCharactersValid, isUpperCaseCharactersValid = checkPasswordPolicy(
+            patchedPassword,
+            upperCaseCharacters,
+            numbers,
+            specialCharacters
+        )
 
-    for _ in range(batch_size):
-        genPsk: str = ""
+        if (
+            isLowerCaseCharactersValid and
+            isNumbersValid and
+            isSpecialCharactersValid and
+            isUpperCaseCharactersValid
+        ):
+            break
 
-        for _ in range(policy.rules.min_upper_case_letters):
-            genPsk += __getUpperAlpha()
+        if not isLowerCaseCharactersValid:
+            patchedPassword = patchCharacters(
+                patchedPassword,
+                "lower",
+                findRequiredLengthToSatisfyPasswordPolicy(
+                    patchedPassword, "lower", 1)
+            )
+        if not isUpperCaseCharactersValid:
+            patchedPassword = patchCharacters(
+                patchedPassword,
+                "upper",
+                findRequiredLengthToSatisfyPasswordPolicy(
+                    patchedPassword, "upper", upperCaseCharacters)
+            )
+        if not isNumbersValid:
+            patchedPassword = patchCharacters(
+                patchedPassword,
+                "numbers",
+                findRequiredLengthToSatisfyPasswordPolicy(
+                    patchedPassword, "numbers", numbers)
+            )
+        if not isSpecialCharactersValid:
+            patchedPassword = patchCharacters(
+                patchedPassword,
+                "special",
+                findRequiredLengthToSatisfyPasswordPolicy(
+                    patchedPassword, "special", specialCharacters)
+            )
 
-        for _ in range(policy.rules.min_lower_case_letters):
-            genPsk += __getAlpha()
-
-        for _ in range(policy.rules.min_digits):
-            genPsk += __getNumber()
-
-        for _ in range(policy.rules.min_symbols):
-            genPsk += __getSymbol()
-
-        while len(genPsk) < policy.rules.min_length:
-            # TODO: Use UUID as the index generator?
-            i = randint(0, len(generators)-1)
-            genPsk += generators[i](policy.allowed_symbols)
-
-        passchars = list(genPsk)
-        shuffle(passchars)
-        genPsk = "".join(passchars)
-        passwords.append(genPsk)
-
-    return passwords
-
-
-def __getAlpha(*args) -> str:
-    alphaMap = "abcdefghijklmnopqrstuvwxyz"
-    return alphaMap[__getRandomIndex(len(alphaMap))]
-
-
-def __getUpperAlpha(*args) -> str:
-
-    capAlphaMap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    return capAlphaMap[__getRandomIndex(len(capAlphaMap))]
-
-
-def __getNumber(*args) -> str:
-    numberMap = "0123456789"
-    return numberMap[__getRandomIndex(len(numberMap))]
-
-
-def __getSymbol(*args) -> str:
-    symbolMap = "!@#$%^&*()_+-={}[];:<>,./?ß!"
-    return symbolMap[__getRandomIndex(len(symbolMap))]
+    return patchedPassword
 
 
-def __getRandomIndex(range: int) -> int:
-    now = str(time.time_ns())
-    randomNumber = int(now[11:13])
-    time.sleep(0.0000001)
-    return randomNumber % range
+def findRequiredLengthToSatisfyPasswordPolicy(
+    password: str,
+    characterClass: str,
+    requiredLength: int
+) -> int:
+    if characterClass == "lower":
+        matches = re.findall(r'[a-z]', password)
+        return abs(len(matches) - requiredLength)
+    elif characterClass == "upper":
+        matches = re.findall(r'[A-Z]', password)
+        return abs(len(matches) - requiredLength)
+    elif characterClass == "numbers":
+        matches = re.findall(r'\d', password)
+        return abs(len(matches) - requiredLength)
+    elif characterClass == "special":
+        specialCharRegex = r'[!@#$%^&*()_+\-={}\[\];:<>,./?ß]'
+        matches = re.findall(specialCharRegex, password)
+        return abs(len(matches) - requiredLength)
+    else:
+        return 0
+
+
+def patchCharacters(
+    password: str,
+    characterClass: str,
+    requiredLength: int
+) -> str:
+    if requiredLength <= 0 or requiredLength > len(password):
+        return password
+
+    allowedCharacters = getAllowedCharactersBasedOnCharacterClass(
+        characterClass)
+
+    patchedPassword = password
+    for _ in range(requiredLength):
+        randomIndex = random.randint(0, len(password) - 1)
+        randomCharacterIndex = random.randint(0, len(allowedCharacters) - 1)
+        randomCharacter = allowedCharacters[randomCharacterIndex]
+
+        patchedPassword = (
+            patchedPassword[:randomIndex] +
+            randomCharacter +
+            patchedPassword[randomIndex + 1:]
+        )
+
+    return patchedPassword
+
+
+def getAllowedCharactersBasedOnCharacterClass(characterClass: str) -> str:
+    if characterClass == "lower":
+        return allowedLowerCaseCharacters
+    elif characterClass == "numbers":
+        return allowedNumbers
+    elif characterClass == "upper":
+        return allowedUpperCaseCharacters
+    elif characterClass == "special":
+        return allowedSpecialCharacters
+    else:
+        return allowedLowerCaseCharacters
+
+
+def generateRandomAllowedCharacterOrNumber() -> str:
+    allowedCharacters = (
+        allowedLowerCaseCharacters +
+        allowedUpperCaseCharacters +
+        allowedNumbers +
+        allowedSpecialCharacters
+    )
+
+    index = random.randint(0, len(allowedCharacters) - 1)
+    return allowedCharacters[index]
+
+
+def checkPasswordPolicy(
+    password: str,
+    upperCaseCharacters: int,
+    numbers: int,
+    specialCharacters: int
+) -> Tuple[bool, bool, bool, bool]:
+    isNumbersValid = validatePasswordNumbers(password, numbers)
+    isSpecialCharactersValid = validatePasswordSpecialCharacters(
+        password, specialCharacters)
+    isLowerCaseCharactersValid = validatePasswordLowerCaseCharacters(
+        password, 1)
+    isUpperCaseCharactersValid = validatePasswordUpperCaseCharacters(
+        password, upperCaseCharacters)
+
+    return (
+        isNumbersValid,
+        isLowerCaseCharactersValid,
+        isUpperCaseCharactersValid,
+        isSpecialCharactersValid
+    )
+
+
+def validatePasswordUpperCaseCharacters(password: str, length: int) -> bool:
+    regex = re.compile(f"([A-Z].*?){{{length},}}")
+    return bool(regex.search(password))
+
+
+def validatePasswordNumbers(password: str, length: int) -> bool:
+    regex = re.compile(f"(\\d.*?){{{length},}}")
+    return bool(regex.search(password))
+
+
+def validatePasswordLowerCaseCharacters(password: str, length: int) -> bool:
+    regex = re.compile(f"([a-z].*?){{{length},}}")
+    return bool(regex.search(password))
+
+
+def validatePasswordSpecialCharacters(password: str, length: int) -> bool:
+    allowedSpecialCharactersInRegixUnderstandableFormat = r"!@#\$%\^&\*\$_\+\-={}\[\];:<>,\./\?ß"
+    regex = re.compile(
+        f"([{allowedSpecialCharactersInRegixUnderstandableFormat}].*?){{{length},}}")
+    return bool(regex.search(password))
